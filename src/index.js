3b9a1a6ae755806c21fa2b818ddebbcf1390dab0
abhinavdhangar <abhinavderapur0@gmail.com>
2022-07-14 21:22:33 +0530

    fixed some changes

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import { ContextProvider } from './pages/Context';
+import { ContextProvider } from './pages/Context';
 import Du from './pages/Du';
-// import App1 from './alag/App';
+import App1 from './alag/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
